3d4f7483242425cc79f593a1fec67b32433a0496
Miangaly <miangaly.randriamifidy@gmail.com>
2021-10-04 17:31:33 +0300

    Fix bug modal

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -1,6 +1,9 @@
 <?php
 namespace App\Controller;
 
+use App\Repository\ArticleRepository;
+use App\Repository\AuthorRepository;
+use App\Repository\CategoryRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,8 +14,13 @@ class AdminController extends AbstractController
      * @Route("/admin", name="admin")
      * 
      */
-    public function index()
+    public function index(ArticleRepository $articleRepository,AuthorRepository $authorRepository,CategoryRepository $categoryRepository)
     {
-        return $this->render('admin/base.html.twig', []);
+        return $this->render('admin/base.html.twig', [
+            'nbArticles' => $articleRepository->countArticles(),
+            'nbCategories' => $categoryRepository->countCategories(),
+            'nbAuthors' => $authorRepository->countAuthors()
+        ]);
     }
+
 }
